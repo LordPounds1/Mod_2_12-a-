6e64363eed40db3177a7484c1347cb4731b3cd0f
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 22:01:17 +0600

    Add project files.

---
+++ Mod_2_12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mod_2_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Все целые числа от 20 до 35: ");
+            for (int i = 20; i <= 35; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
